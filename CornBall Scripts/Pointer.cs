313626cc99f0c5d5308d531818b9b8de56b25cd2
Rohan Choudhary <rohan5210work@gmail.com>
2025-07-17 19:48:39 +0530

    Add files via upload

---
+++ CornBall Scripts/Pointer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public static Pointer instance;
+    public bool hasCornHole = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && hasCornHole == false)
+        {
+            if (collisions.instance.hasCollided == true)
+            {
+                GameManager.instance.score += 2;
+            }
+            else
+            {
+                GameManager.instance.score += 3;
+            }
+            hasCornHole = true;
+        }
+    }
+}
