a0cf034925ecb8e180173251eb0ef575699416c2
Rohan Choudhary <rohan5210work@gmail.com>
2025-07-23 08:13:59 +0530

    Add files via upload

---
+++ CornBall Scripts/CorneTest/GroundDetector.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+/// <summary>
+/// Detects when the sandbag hits the ground.
+/// It updates the sandbag's state directly.
+/// Renamed from 'groundhit.cs'.
+/// </summary>
+public class GroundDetector : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the object that hit is the sandbag
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SandbagController sandbag = collision.gameObject.GetComponent<SandbagController>();
+            
+            // If we found a sandbag component and it hasn't already been marked as hitting the ground
+            if (sandbag != null && !sandbag.HasHitGround)
+            {
+                Debug.Log("Sandbag hit the ground.");
+                sandbag.HasHitGround = true;
+            }
+        }
+    }
+}
